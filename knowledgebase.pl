36f185d299506a20976e11437632fd2934dec6a1
Ezra Jeonadab G. Del Rosario <ezrajeonadabg@gmail.com>
2024-11-27 16:13:32 +0800

    modified knowledge base, added predicates for asserts

--- knowledgebase.pl
+++ knowledgebase.pl
@@ -2,14 +2,14 @@
 :- dynamic glitter/2.
 :- dynamic pit/2.
 :- dynamic gold/2.
-:- dynamic safe/2.
+:- dynamic unexplored_safe/2.
+:- dynamic explored_safe/2.
 :- dynamic home/2.
 :- dynamic unknown/2.
 
 tae(maasim).
-
 assert_fact(N) :-
-    not(N), writeln(N),
+    not(N), %writeln(N),
     assertz(N).
 
 in_bounds(R,C,N) :-
@@ -18,14 +18,33 @@ in_bounds(R,C,N) :-
 
 initialize_start(R,C,N) :-
     assert_fact(home(R,C)),
-    assert_fact(safe(R,C)),
+    assert_fact(explored_safe(R,C)),
+
     forall(
     (
         member((DR, DC), [(-1, 0), (1, 0), (0, -1), (0, 1)]),  % Offsets for neighbors
         NR is R + DR,
         NC is C + DC,
         in_bounds(NR,NC,N)
-    ),  assert_fact(safe(NR,NC))).
+    ),  assert_fact(unexplored_safe(NR,NC))).
+
+assert_explored_safe(R,C,N) :-
+    (unexplored_safe(R, C); unknown(R, C)),
+    (retract(unexplored_safe(R,C)); retract(unknown(R,C))),
+    assert_fact(explored_safe(R,C)),
+    assert_unexplored_safe_adjacent(R,C,N).
+
+assert_breeze(R,C,N) :-
+    unexplored_safe(R, C),
+    retract(unexplored_safe(R,C)),
+    assert_fact(breeze(R,C)),
+    assert_unknown_adjacent(R,C,N).
+
+assert_glitter(R,C,N) :-
+    unexplored_safe(R, C),
+    retract(unexplored_safe(R,C)),
+    assert_fact(glitter(R,C)),
+    assert_unknown_adjacent(R,C,N).
 
 assert_unknown_adjacent(R,C,N) :-
     (breeze(R,C); glitter(R,C)), 
@@ -35,11 +54,13 @@ assert_unknown_adjacent(R,C,N) :-
         NR is R + DR,
         NC is C + DC,
         in_bounds(NR,NC,N),
-        not(safe(NR,NC))
+        not(explored_safe(NR,NC)),
+        not(unexplored_safe(NR,NC)),   
+        not(home(NR,NC))
     ),  assert_fact(unknown(NR,NC))).
 
-assert_safe_adjacent(R,C,N) :-
-    safe(R,C), not(breeze(R,C)), not(glitter(R,C)),
+assert_unexplored_safe_adjacent(R,C,N) :-
+    explored_safe(R,C), not(breeze(R,C)), not(glitter(R,C)),
     forall(
     (
         member((DR, DC), [(-1, 0), (1, 0), (0, -1), (0, 1)]),  % Offsets for neighbors
@@ -47,20 +68,14 @@ assert_safe_adjacent(R,C,N) :-
         NC is C + DC,
         in_bounds(NR,NC,N),
         not(home(NR,NC)),
-        not(unknown(NR,NC))
-    ),  assert_fact(safe(NR,NC))).
+        not(unknown(NR,NC)),
+        not(explored_safe(NR, NC))
+    ),  assert_fact(unexplored_safe(NR,NC))).
 
 
-% get_pit(R,C,N) :- % Assuming the position entered is a breeze.
+%get_pit(R,C,N) :- % Assuming the position entered is a breeze.
 
-replace_unknown(R,C) :-
-    unknown(R,C),
-    retract(unknown(R,C)),
-    assert_fact(safe(R,C)).
-
-% replace_unknown_safe
-
-end(R,C) :-
+fall(R,C) :-
     pit(R,C).
 
 grab(R,C) :-
